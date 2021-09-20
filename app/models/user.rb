8bbc80d89db0cb0d4ec91be97194442d15de3303
Drew Kang <kangdrew95@gmail.com>
2021-09-20 22:16:09 +0000

    Email is not case sensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,7 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true
-  validates :email, uniqueness: true
+  validates :first_name, :last_name, :email, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
 
 end
